05ff33aeb701a775a0ad502972e8b199a7b6ab0f
Sidra ELEzz <76815358+SidraELEzz@users.noreply.github.com>
2021-10-26 22:38:25 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,54 @@
+import os , codecs
+
+from setuptools import setup, find_packages
+
+pkg = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(pkg, "README.md"), encoding="utf-8") as fh:
+	
+    long_description = "\n" + fh.read()
+
+
+setup(
+
+    name="InstagramIG",
+    
+    version="0.0.2",
+    
+    author="SidraELEzz",
+    
+    author_email="",
+    
+    description = ("Scrape Instagram Profile and login Instagram"),
+    
+    long_description_content_type="text/markdown",
+    
+    url="https://github.com/SidraELEzz/InstagramIG",
+    
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    
+    long_description=long_description,
+    
+    packages=find_packages(),
+    
+    install_requires=['requests'],
+    
+    keywords=['instagram'],
+    
+    classifiers=[
+    
+        "Development Status :: 1 - Planning",
+        
+        "Intended Audience :: Developers",
+        
+        "Programming Language :: Python :: 3",
+        
+        "Operating System :: Unix",
+        
+        "Operating System :: MacOS :: MacOS X",
+        
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
